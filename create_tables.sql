675370c2f0d93301ed4c45b5a75b46a909c63134
goli <shirafkanzeynab63@gmail.com>
2025-05-23 17:13:19 +0330

    create professor table

--- create_tables.sql
+++ create_tables.sql
@@ -13,3 +13,12 @@ create table Student(
     foreign key (user_id) peferences User(user_id)
 );
 
+create table Professor(
+    professor_id int primary key ,
+    user_id int not null ,
+    personnel_number varchar(12) unique not null ,
+    department varchar(30) not null ,
+    academic_rank varchar(20) not null ,
+    foreign key (user_id) peferences User(user_id)
+);
+
